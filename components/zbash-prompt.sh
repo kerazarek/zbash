fc67aa32def0a772616ce35029e4e019d6d717ce
Zarek Siegel <zarek.siegel@gmail.com>
2016-10-27 17:38:38 -0400

    end of day October 27, 2016 5:38 PM

--- components/zbash-prompt.sh
+++ components/zbash-prompt.sh
@@ -33,26 +33,45 @@ PS1=\
 
 # compfmt=$(pyform -c 0 -o -f 88 -b 201 -n -t )
 # [[ $COMP == 'fido' ]] && compfmt=$(pyform -c 0 -o -f 88 -b 201 -n -t )
-if [[ $COMP == 'fido' ]]; then
-	compfmt=$(pyform -c 0 -o -f 18 -b 33 -n -t )
-elif [[ $COMP == 'pascal' ]]; then
-	compfmt=$(pyform -c 0 -o -f 22 -b 48 -n -t )
-elif [[ $COMP == 'panda' ]]; then
-	compfmt=$(pyform -c 0 -o -f 52 -b 9 -n -t )
-elif [[ $COMP =~ fido_node.+ ]]; then
-	compfmt=$(pyform -c 0 -o -f 18 -b 33 -v -n -t )
-elif [[ $COMP =~ panda_node.+ ]]; then
-	compfmt=$(pyform -c 0 -o -f 52 -b 9 -v -n -t )
-else
-	compfmt=$(pyform -c 0 -o -f 88 -b 201 -n -t )
-fi
 
+compfmt=$(pyform -c 0 -o -f 88 -b 201 -n -t )
 datefmt=$(pyform -c 0 -f 208 -b def -n -t )
 cwdfmt=$(pyform -c 0 -f 226 -b def -n -t )
 histfmt=$(pyform -c 0 -f 2 -b def -n -t )
 arrowfmt=$(pyform -c 0 -o -f 46 -b def -n -t )
 promptfmt=$(pyform -c 0 -f 51 -b def -n -t )
 clearfmt=$(pyform -c 2 -n -t)
+
+
+if [[ $COMP =~ ^fido.* ]]; then
+	compfmt=$(pyform -c 0 -o -f 18 -b 33 -n -t )
+	datefmt=$(pyform -c 0 -f 129 -b def -n -t )
+	cwdfmt=$(pyform -c 0 -f 124 -b def -n -t )
+	histfmt=$(pyform -c 0 -f 11 -b def -n -t )
+	arrowfmt=$(pyform -c 0 -o -f 226 -b def -n -t )
+	# promptfmt=$(pyform -c 0 -f 46 -b def -n -t )
+elif [[ $COMP == 'pascal' ]]; then
+	compfmt=$(pyform -c 0 -o -f 22 -b 48 -n -t )
+	datefmt=$(pyform -c 0 -f 51 -b def -n -t )
+	cwdfmt=$(pyform -c 0 -f 129 -b def -n -t )
+	histfmt=$(pyform -c 0 -f 124 -b def -n -t )
+	arrowfmt=$(pyform -c 0 -o -f 11 -b def -n -t )
+	# promptfmt=$(pyform -c 0 -f 226 -b def -n -t )
+elif [[ $COMP =~ ^panda.* ]]; then
+	compfmt=$(pyform -c 0 -o -f 52 -b 9 -n -t )
+	datefmt=$(pyform -c 0 -f 129 -b def -n -t )
+	cwdfmt=$(pyform -c 0 -f 51 -b def -n -t )
+	histfmt=$(pyform -c 0 -f 46 -b def -n -t )
+	arrowfmt=$(pyform -c 0 -o -f 226 -b def -n -t )
+	# promptfmt=$(pyform -c 0 -f 208 -b def -n -t )
+fi
+
+if [[ $COMP =~ fido_node.+ ]]; then
+	compfmt=$(pyform -c 0 -o -f 18 -b 33 -v -n -t )
+elif [[ $COMP =~ panda_node.+ ]]; then
+	compfmt=$(pyform -c 0 -o -f 52 -b 9 -v -n -t )
+fi
+
 
 PS1=${PS1/\{compfmt\}/$compfmt}
 PS1=${PS1/\{datefmt\}/$datefmt}
